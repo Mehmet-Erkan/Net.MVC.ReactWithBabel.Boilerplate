776cc70ac975b96af1f43184974e303e17d6d7a7
Mehmet Erkan <mehmet.erkan.ch@outlook.com>
2019-07-29 10:25:57 +0200

    Add project files.

---
+++ Net.MVC.ReactWithBabel.Boilerplate/Scripts/React/app.jsx
@@ -0,0 +1,15 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const myName = "Mehmet";
+
+// Simple React Component
+ReactDOM.render(
+    <h1>Hello {myName} </h1>,
+    document.getElementById('reactTypeScriptContainer')
+);
+
+//function StringOutput(YourName: string) {
+//    return <h1>Hello {myName} </h1>
+//}
+
---
+++ Net.MVC.ReactWithBabel.Boilerplate/Scripts/React/webpack.config.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: "./app.jsx",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env', 'babel-preset-react']
+                    }
+                }
+            }
+        ]
+    }
+}
